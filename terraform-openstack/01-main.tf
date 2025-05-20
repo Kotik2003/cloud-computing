f0021c4eea6b94cd146eb44a5a843bcde901a837
Kate Vazhenina <139289903+Kotik2003@users.noreply.github.com>
2025-05-20 12:07:52 +0300

    Update 01-main.tf

--- terraform-openstack/01-main.tf
+++ terraform-openstack/01-main.tf
@@ -8,44 +8,48 @@ terraform {
   }
 }
 
+
+# use environment variables
+# https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs
 provider "openstack" {
-  auth_url    = "https://cloud.crplab.ru:5000"
-  tenant_id   = "a02aed7892fa45d0bc2bef3b8a08a6e9"
+  auth_url = "https://cloud.crplab.ru:5000"
+  tenant_id = "a02aed7892fa45d0bc2bef3b8a08a6e9"
   tenant_name = "students"
   user_domain_name = "Default"
-  user_name   = var.user_name
-  password    = var.password
-  region      = "RegionOne"
+  # export TF_VAR_user_name=
+  user_name = var.user_name
+  # export TF_VAR_password=
+  password = var.password
+  region = "RegionOne"
 }
 
+# https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/networking_secgroup_v2
 resource "openstack_networking_secgroup_v2" "sg" {
   name = "vazhenina-group-trfm"
 }
 
+# https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/networking_secgroup_rule_v2
 resource "openstack_networking_secgroup_rule_v2" "sg_ssh_rule" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
-  remote_ip_prefix  = "0.0.0.0/0"
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol = "tcp"
+  port_range_min = 22
+  port_range_max = 22
+  remote_ip_prefix = "0.0.0.0/0"
   security_group_id = openstack_networking_secgroup_v2.sg.id
 }
 
-# Добавляем ключ
-resource "openstack_compute_keypair_v2" "vazhenina_key" {
-  name       = var.key_pair
-  public_key = file("~/.ssh/id_rsa.pub")
-}
-
+# https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/compute_instance_v2
 resource "openstack_compute_instance_v2" "vazhenina_server" {
-  name           = "vazhenina-server-trfm"
-  image_name     = var.image_name
-  flavor_name    = var.server_flavor
-  key_pair       = openstack_compute_keypair_v2.vazhenina_key.name
+  name = "vazhenina-server-trfm"
+  image_name = var.image_name
+  flavor_name = var.server_flavor
+  key_pair =  "vazhenina"
   security_groups = [openstack_networking_secgroup_v2.sg.id]
 
   network {
     name = var.network_name
   }
 }
+
+# https://github.com/diodonfrost/terraform-openstack-examples
