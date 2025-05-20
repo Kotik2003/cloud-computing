f59086b014e2aebb77941c7f9d13bf0ac05f6894
Kate Vazhenina <139289903+Kotik2003@users.noreply.github.com>
2025-05-20 09:01:28 +0300

    Update 02-output.tf

--- terraform-openstack/02-output.tf
+++ terraform-openstack/02-output.tf
@@ -1,5 +1,5 @@
 output "servers" {
   value = {
-    serverip = openstack_compute_instance_v2.filonov_server.access_ip_v4
+    serverip = openstack_compute_instance_v2.vazhenina_server.access_ip_v4
   }
 }
